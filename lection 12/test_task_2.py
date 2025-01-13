138195dcd92edd3a77b721c230e8d6388cb20f8c
aa.vilinskiy <lt26iw@gmail.com>
2025-01-13 11:21:22 +0300

    Выделил авторизацию в отдельный файл, чтобы переиспользовать

--- lection 12/test_task_2.py
+++ lection 12/test_task_2.py
@@ -6,3 +6,35 @@
 # 6. Создать новую папку и перейти в неё
 # 7. Убедиться, что она пустая
 # 8. Удалить новую папку, проверить, что её нет в списке папок
+
+from atf import *
+from atf.pytest_core.base.case_ui import TestCaseUI
+from atf.ui import Region, TextField, Button, ExactText, CustomList, UrlExact, Enabled, Displayed, Element, \
+    ContainsText, CountElements, UrlContains
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys
+
+from Auto_auth import *
+
+class MainPage(Region):
+    pass
+
+class TaskPage(Region):
+    pass
+
+
+class TestTaskRegistry(TestCaseUI):
+
+    def test_task_registry(self):
+        sbis_site = self.config.get("SBIS_SITE")
+
+        log(f"Переходим на сайт {sbis_site}")
+        self.browser.open(sbis_site)
+
+        log("Авторизовываемся")
+        auth_page = AuthPage(self.driver)
+        auth_page.authorize()
+
+        log("Переходим в реестр Задачи на вкладку В работе ")
+        task_page = TaskPage(self.driver)
